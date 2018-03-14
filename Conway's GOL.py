878fe576ffa8bd17843e2ec5c520f6649d8b8d5d
Jordan <jordan.camilletti@gmail.com>
2018-03-13 22:38:56 -0500

    Initial commit

---
+++ Conway's GOL.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+done=False
+
+#clock = pygame.time.Clock()
+
+while(not done):
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			done=True
+
+		pygame.display.flip()
+		#clock.tick(60)
