06601193d6547aa3afe96bd58a517bba012cb1df
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-03-15 02:53:07 -0500

    Created basic grid

--- Conway's GOL.py
+++ Conway's GOL.py
@@ -1,15 +1,21 @@
 import pygame
 
 pygame.init()
+white=(255,255,255)
+black=(0,0,0)
 screen=pygame.display.set_mode((400, 300))
+finish=False
 pygame.display.set_caption("Conway's Game of Life.")
+grid=[[0,0,0],[0,0,0]]
 #clock = pygame.time.Clock()
 
-while(True):
+while(not finish):
 	for event in pygame.event.get():
 		if(event.type == pygame.QUIT):
-			pygame.quit()
+			finish=True
 
-		pygame.display.update()
-		#clock.tick(60)
+	#pygame.draw.rect(screen,white,[200,200,10,10])
+	pygame.display.update()
+	#clock.tick(60)
+pygame.quit()
 quit()
