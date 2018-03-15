01a396534d3b885453709f6905980fa7eb07745a
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-03-14 21:07:25 -0500

    Updated title

--- Conway's GOL.py
+++ Conway's GOL.py
@@ -2,14 +2,14 @@ import pygame
 
 pygame.init()
 screen=pygame.display.set_mode((400, 300))
-done=False
-
+pygame.display.set_caption("Conway's Game of Life.")
 #clock = pygame.time.Clock()
 
-while(not done):
+while(True):
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			done=True
+		if(event.type == pygame.QUIT):
+			pygame.quit()
 
-		pygame.display.flip()
+		pygame.display.update()
 		#clock.tick(60)
+quit()
