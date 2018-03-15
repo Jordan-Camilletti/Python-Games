05bac178ba734a5a15797e6309fb6dc80a7789e3
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-03-15 03:14:12 -0500

    Grid is now shown, true squares are lit and grid

--- Conway's GOL.py
+++ Conway's GOL.py
@@ -1,12 +1,14 @@
+#"Conway's GOL".py
 import pygame
 
 pygame.init()
 white=(255,255,255)
 black=(0,0,0)
-screen=pygame.display.set_mode((400, 300))
 finish=False
+grid=[[1,0,1],[0,1,0]]
+yPos,xPos=0,0
+screen=pygame.display.set_mode((400, 300))
 pygame.display.set_caption("Conway's Game of Life.")
-grid=[[0,0,0],[0,0,0]]
 #clock = pygame.time.Clock()
 
 while(not finish):
@@ -14,7 +16,16 @@ while(not finish):
 		if(event.type == pygame.QUIT):
 			finish=True
 
-	#pygame.draw.rect(screen,white,[200,200,10,10])
+	yPos=-1
+	for y in grid:
+		yPos+=1
+		xPos=-1
+		for x in y:
+			xPos+=1
+			if(x):
+				pygame.draw.rect(screen,white,[xPos*5,yPos*5,5,5])#x,y,width,height
+			else:
+				pygame.draw.rect(screen,black,[xPos*5,yPos*5,5,5])
 	pygame.display.update()
 	#clock.tick(60)
 pygame.quit()
