03f827a681a568b90e2c4f2a3959515528b959ff
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-03-15 14:54:16 -0500

    Basic pause/unpause features implemented

--- Conway's GOL.py
+++ Conway's GOL.py
@@ -5,6 +5,7 @@ pygame.init()
 white=(255,255,255)
 black=(0,0,0)
 finish=False
+pause=True
 grid=[[0]*80 for n in range(60)]#60 arrays that are size 80
 xPos,yPos=0,0
 screen=pygame.display.set_mode((400, 300))
@@ -13,8 +14,13 @@ pygame.display.set_caption("Conway's Game of Life.")
 
 while(not finish):
 	for event in pygame.event.get():
+		#print(event)
 		if(event.type == pygame.QUIT):
 			finish=True
+		if(event.type == pygame.MOUSEBUTTONDOWN):
+			pause=False
+		if(event.type == pygame.MOUSEBUTTONUP):
+			pause=True
 
 	yPos=-1
 	for y in grid:
