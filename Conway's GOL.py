6e62848a993a87da4a247a5ab32962c69998a303
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-03-16 18:14:31 -0500

    Removed some unused comments

--- Conway's GOL.py
+++ Conway's GOL.py
@@ -1,6 +1,6 @@
-#"Conway's GOL".py
+"""Conway's GOL".py
 
-"""For a space that is 'populated':
+For a space that is 'populated':
 Each cell with one or no neighbors dies, as if by solitude.
 Each cell with four or more neighbors dies, as if by overpopulation.
 Each cell with two or three neighbors survives.
