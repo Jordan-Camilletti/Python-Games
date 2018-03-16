8079a13bd6f6f46bccb164745aa4033d783e6082
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-03-15 19:05:42 -0500

    Can now add/remove blocks while paused

--- Conway's GOL.py
+++ Conway's GOL.py
@@ -1,4 +1,10 @@
 #"Conway's GOL".py
+"""For a space that is 'populated':
+Each cell with one or no neighbors dies, as if by solitude.
+Each cell with four or more neighbors dies, as if by overpopulation.
+Each cell with two or three neighbors survives.
+For a space that is 'empty' or 'unpopulated'
+Each cell with three neighbors becomes populated."""
 import pygame
 from math import floor
 
@@ -7,11 +13,15 @@ white=(255,255,255)
 black=(0,0,0)
 finish=False
 pause=True
-grid=[[0]*80 for n in range(60)]#60 arrays that are size 80
+grid=[[False]*80 for n in range(60)]#60 arrays that are size 80
 xPos,yPos=0,0
 screen=pygame.display.set_mode((400, 300))
 pygame.display.set_caption("Conway's Game of Life.")
 #clock = pygame.time.Clock()
+
+def populate(grid,x,y):
+	return(0)
+
 
 while(not finish):
 	for event in pygame.event.get():
@@ -23,18 +33,23 @@ while(not finish):
 				pause=not pause
 		if(event.type == pygame.MOUSEBUTTONDOWN):#clicking on square flips it
 			mouseX,mouseY=pygame.mouse.get_pos()
-			grid[floor(mouseY/5)][floor(mouseX/5)]=1-grid[floor(mouseY/5)][floor(mouseX/5)]
-
-	yPos=-1
-	for y in grid:
-		yPos+=1
-		xPos=-1
-		for x in y:
-			xPos+=1
-			if(x):
-				pygame.draw.rect(screen,white,[xPos*5,yPos*5,5,5])#x,y,width,height
+			grid[floor(mouseY/5)][floor(mouseX/5)]=not grid[floor(mouseY/5)][floor(mouseX/5)]
+			if(grid[floor(mouseY/5)][floor(mouseX/5)]):
+				pygame.draw.rect(screen,white,[floor(mouseX/5)*5,floor(mouseY/5)*5,5,5])
 			else:
-				pygame.draw.rect(screen,black,[xPos*5,yPos*5,5,5])
+				pygame.draw.rect(screen,black,[floor(mouseX/5)*5,floor(mouseY/5)*5,5,5])
+
+	if(not pause):
+		yPos=-1
+		for y in grid:
+			yPos+=1
+			xPos=-1
+			for x in y:
+				xPos+=1
+				if(x):
+					pygame.draw.rect(screen,white,[xPos*5,yPos*5,5,5])#x,y,width,height
+				else:
+					pygame.draw.rect(screen,black,[xPos*5,yPos*5,5,5])
 	pygame.display.update()
 	#clock.tick(60)
 pygame.quit()
