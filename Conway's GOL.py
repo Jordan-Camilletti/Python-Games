e25c5dc0065f098c3c162b0e7418514dabf76a70
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-03-19 15:11:31 -0500

    Update Conway's GOL.py

--- Conway's GOL.py
+++ Conway's GOL.py
@@ -5,7 +5,7 @@ Each cell with one or no neighbors dies, as if by solitude.
 Each cell with four or more neighbors dies, as if by overpopulation.
 Each cell with two or three neighbors survives.
 
-For a space that is 'empty' or 'unpopulated'
+For a space that is 'empty' or 'unpopulated':
 Each cell with three neighbors becomes populated."""
 import pygame
 from math import floor
