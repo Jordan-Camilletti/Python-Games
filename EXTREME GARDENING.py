b8fa9b4bde65de8baa59de6f70b8ab203480a206
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-03-20 23:58:34 -0500

    Added basic vars

--- EXTREME GARDENING.py
+++ EXTREME GARDENING.py
@@ -6,6 +6,9 @@ import pygame
 
 pygame.init()
 finish=False
+dosh=0
+years=0
+name=""
 screen=pygame.display.set_mode((400,300))
 pygame.display.set_caption("EXTREME GARDENING")
 
@@ -13,7 +16,7 @@ while(not finish):
 	for event in pygame.event.get():
 		if(event.type == pygame.QUIT):
 			finish=True
-			
+
 	pygame.display.update()
 pygame.quit()
 quit()
