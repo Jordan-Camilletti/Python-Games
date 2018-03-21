db5e71db4daf0d2e5ad088ee461271b06aca29af
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-03-20 23:31:49 -0500

    Fixing basic skeleton

--- EXTREME GARDENING.py
+++ EXTREME GARDENING.py
@@ -7,7 +7,7 @@ import pygame
 pygame.init()
 finish=False
 screen=pygame.display.set_mode((400,300))
-pygame.display.set_captions("EXTREME GARDENING")
+pygame.display.set_caption("EXTREME GARDENING")
 
 while(not finish):
 	finish=True
