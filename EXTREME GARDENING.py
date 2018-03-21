4c3e195b2c8f7b9f54bc0411a1dc28938c603b5e
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-03-20 23:31:01 -0500

    Added basic pygame skeleton

--- EXTREME GARDENING.py
+++ EXTREME GARDENING.py
@@ -2,3 +2,14 @@
 
 """
 """
+import pygame
+
+pygame.init()
+finish=False
+screen=pygame.display.set_mode((400,300))
+pygame.display.set_captions("EXTREME GARDENING")
+
+while(not finish):
+	finish=True
+pygame.quit()
+quit()
