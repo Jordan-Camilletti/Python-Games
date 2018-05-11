fe6f98f293b97635ae93a6d8a16b80eea642a5eb
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-05-10 23:03:08 -0500

    For loop used to readuce code

--- Sierpinski Carpet/main.py
+++ Sierpinski Carpet/main.py
@@ -25,8 +25,8 @@ while(not finish):
 	if(not pause):
 		turn+=1
 		t=findSize(turn)
-		pygame.draw.rect(screen,white,[t,t,t,t])
-		print(9**(turn-1))
+		for n in range(9**(turn-1)):
+			pygame.draw.rect(screen,white,[t,t,t,t])
 		"""pygame.draw.rect(screen,white,[900/(3**1),900/(3**1),900/(3**1),900/(3**1)])
 		pygame.draw.rect(screen,white,[900/(3**2),900/(3**2),900/(3**2),900/(3**2)])
 		pygame.draw.rect(screen,white,[900/(3**3),900/(3**3),900/(3**3),900/(3**3)])"""
