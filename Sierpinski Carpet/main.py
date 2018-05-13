8805743a3d93d964276c06497a5109505ce0c01f
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-05-13 15:43:04 -0500

    Squares now good

--- Sierpinski Carpet/main.py
+++ Sierpinski Carpet/main.py
@@ -18,7 +18,7 @@ pause=False
 screen=pygame.display.set_mode((900, 900))
 pygame.display.set_caption("Sierpinski's Carpet")
 
-while(not finish):
+while(turn<3):
 	for event in pygame.event.get():
 		if(event.type==pygame.QUIT):
 			finish=True
@@ -29,12 +29,12 @@ while(not finish):
 	if(not pause):
 		turn+=1
 		t=findSize(turn)
-		#print(9**(turn-1))
-		#print(t)
-		for n in range(9**(turn-1)):
-			#print(t+(math.floor(n%3)*findSize(turn-1)),t+(math.floor(n/3)*findSize(turn-1)))
-		#print("\n")
-			pygame.draw.rect(screen,white,[t+(math.floor(n%3)*findSize(turn-1)),t+(math.floor(n/3)*findSize(turn-1)),t,t])
+		for n1 in range(int(9**((turn-1)/2))):
+			for n2 in range(int(9**((turn-1)/2))):
+				print(t+(n1*findSize(turn-1)), t+(n2*findSize(turn-1)))
+				pygame.draw.rect(screen,white,[t+(n1*findSize(turn-1)), t+(n2*findSize(turn-1)),t,t])
+			print("X")
+		print("\n")
 		"""pygame.draw.rect(screen,white,[300,300,300,300])
 		pygame.draw.rect(screen,white,[100,100,100,100])
 		pygame.draw.rect(screen,white,[400,100,100,100])
@@ -45,9 +45,6 @@ while(not finish):
 		pygame.draw.rect(screen,white,[133,33,33,33])
 		pygame.draw.rect(screen,white,[33,133,33,33])"""
 
-		"""pygame.draw.rect(screen,white,[900/(3**1),900/(3**1),900/(3**1),900/(3**1)])
-		pygame.draw.rect(screen,white,[900/(3**2),900/(3**2),900/(3**2),900/(3**2)])
-		pygame.draw.rect(screen,white,[900/(3**3),900/(3**3),900/(3**3),900/(3**3)])"""
 		pygame.time.wait(1000)
 	pygame.display.update()
 	
