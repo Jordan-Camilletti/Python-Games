e6f0d972d294b3af489588af697ed9b215ae2fa8
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-05-13 15:55:36 -0500

    Game screen now changable

--- Sierpinski Carpet/main.py
+++ Sierpinski Carpet/main.py
@@ -6,19 +6,20 @@ import pygame
 import math
 
 def findSize(t):
-	return(math.floor(900/(3**t)))
+	return(math.floor(size/(3**t)))
 	
+size=int(input("Enter the size of the screen.\n"))
 pygame.init()
 white=(255,255,255)
 black=(0,0,0)
 turn=0
-currSize=300
+currSize=math.sqrt(size/3)
 finish=False
 pause=False
-screen=pygame.display.set_mode((900, 900))
+screen=pygame.display.set_mode((size, size))
 pygame.display.set_caption("Sierpinski's Carpet")
 
-while(turn<3):
+while(not finish and turn<6):
 	for event in pygame.event.get():
 		if(event.type==pygame.QUIT):
 			finish=True
@@ -31,20 +32,7 @@ while(turn<3):
 		t=findSize(turn)
 		for n1 in range(int(9**((turn-1)/2))):
 			for n2 in range(int(9**((turn-1)/2))):
-				print(t+(n1*findSize(turn-1)), t+(n2*findSize(turn-1)))
 				pygame.draw.rect(screen,white,[t+(n1*findSize(turn-1)), t+(n2*findSize(turn-1)),t,t])
-			print("X")
-		print("\n")
-		"""pygame.draw.rect(screen,white,[300,300,300,300])
-		pygame.draw.rect(screen,white,[100,100,100,100])
-		pygame.draw.rect(screen,white,[400,100,100,100])
-		pygame.draw.rect(screen,white,[700,100,100,100])
-		pygame.draw.rect(screen,white,[100,100,100,100])
-		pygame.draw.rect(screen,white,[700,700,100,100])
-		pygame.draw.rect(screen,white,[33,33,33,33])
-		pygame.draw.rect(screen,white,[133,33,33,33])
-		pygame.draw.rect(screen,white,[33,133,33,33])"""
-
 		pygame.time.wait(1000)
 	pygame.display.update()
 	
