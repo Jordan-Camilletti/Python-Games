8848e9405156b1593e24e67f10cfe8d5dcf6b8e9
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-07-09 22:31:58 -0500

    Colors now stack in a better order

--- Binary Counter/main.py
+++ Binary Counter/main.py
@@ -10,17 +10,17 @@ black=(0,0,0)
 #6squares x 4squares
 count=0
 yLen=100
-xLen=yLen*8.86
+xLen=int(yLen*8.86)
 screen=pygame.display.set_mode((xLen,yLen))
 screen.fill(white)
 
 while(not finish):
 	for event in pygame.event.get():
-		if(event.type == pygame.QUIT):
+		if(event.type==pygame.QUIT):
 			finish=True
 
 	count+=1
-	bCount="{0:b}".format(count)
+	bCount=("{0:b}".format(count))[::-1]
 	#print(bCount)
 	for n in range(len(bCount)):
 		if(bCount[n]=="0"):
