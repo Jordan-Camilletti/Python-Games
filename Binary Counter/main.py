83356dcf5996ba61feadd7fe8eccd67635bb4232
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-07-12 23:46:50 -0500

    Timer now is shown

--- Binary Counter/main.py
+++ Binary Counter/main.py
@@ -6,15 +6,19 @@ pygame.init()
 
 #8.86pix x 1pix
 #6squares x 4squares
+white=(255,255,255)
+black=(0,0,0)
 finish=False
 count=0
 yLen=int(input("Enter window height\n"))
 ySqr=int(yLen/4)
 xLen=int(yLen*8.86)
 xSqr=int(xLen/6)
+
 timer=time.time()
+font=pygame.font.SysFont('Arial', 10)
 screen=pygame.display.set_mode((xLen,yLen))
-screen.fill((255,255,255))
+screen.fill(white)
 
 while(not finish):
 	for event in pygame.event.get():
@@ -26,9 +30,10 @@ while(not finish):
 	bCount=("{0:b}".format(count))[::-1]
 	for n in range(len(bCount)):
 		if(bCount[n]=="0"):
-			pygame.draw.rect(screen, (255,255,255), [int(n/4)*xSqr,(n%4)*ySqr,xSqr,ySqr])
+			pygame.draw.rect(screen, white, [int(n/4)*xSqr,(n%4)*ySqr,xSqr,ySqr])
 		elif(bCount[n]=="1" and screen.get_at((int(n/4)*xSqr,(n%4)*ySqr)) == (255,255,255,255)):#This last part makes colors less 'ravey'
 			pygame.draw.rect(screen, (randint(0,255),randint(0,255),randint(0,255)), [int(n/4)*xSqr,(n%4)*ySqr,xSqr,ySqr])
+	screen.blit(font.render(timer,False,black), (0,0))
 
 	pygame.time.wait(100)
 	pygame.display.update()
