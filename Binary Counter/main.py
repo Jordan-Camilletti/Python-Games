52f688c5b2021f0e94f523d77d4497617046d593
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-07-17 22:12:23 -0500

    Space now pauses/unpauses

--- Binary Counter/main.py
+++ Binary Counter/main.py
@@ -9,12 +9,11 @@ from random import randint
 
 pygame.init()
 
-#8.86pix x 1pix
-#6squares x 4squares
 white=(255,255,255)
 black=(0,0,0)
 finish=False
-count=2000
+pause=False
+count=0
 yLen=int(input("Enter window height\n"))
 ySqr=int(yLen/5)
 xLen=int(yLen*8.86)
@@ -31,20 +30,24 @@ while(not finish):
 	for event in pygame.event.get():
 		if(event.type==pygame.QUIT):
 			finish=True
+		if(event.type == pygame.KEYDOWN):#Space=pause/unpause
+			if(event.key == pygame.K_SPACE):
+				pause=not pause
 
-	count+=1
-	bCount=("{0:b}".format(count))[::-1]
-	for n in range(len(bCount)):
-		if(bCount[n]=="0"):
-			pygame.draw.rect(screen, white, [int(n/5)*xSqr,(n%5)*ySqr,xSqr,ySqr])
-		elif(bCount[n]=="1" and screen.get_at((int(n/5)*xSqr,(n%5)*ySqr)) == (255,255,255,255)):#This last part makes colors less 'ravey'
-			pygame.draw.rect(screen, (randint(0,255),randint(0,255),randint(0,255)), [int(n/5)*xSqr,(n%5)*ySqr,xSqr,ySqr])
-	
-	timeS=str(time.time()-timer).split(".")
-	timeStr=timeS[0]+"."+timeS[1][:2]#This makes sure that the time is cut off to 2 decimal places
+	if(not pause):
+		count+=1
+		bCount=("{0:b}".format(count))[::-1]
+		for n in range(len(bCount)):
+			if(bCount[n]=="0"):
+				pygame.draw.rect(screen, white, [int(n/5)*xSqr,(n%5)*ySqr,xSqr,ySqr])
+			elif(bCount[n]=="1" and screen.get_at((int(n/5)*xSqr,(n%5)*ySqr)) == (255,255,255,255)):#This last part makes colors less 'ravey'
+				pygame.draw.rect(screen, (randint(0,255),randint(0,255),randint(0,255)), [int(n/5)*xSqr,(n%5)*ySqr,xSqr,ySqr])
+		
+		timeS=str(time.time()-timer).split(".")
+		timeStr=timeS[0]+"."+timeS[1][:2]#This makes sure that the time is cut off to 2 decimal places
 
-	pygame.draw.rect(screen, screen.get_at((2*xSqr+1,2*ySqr+1)), [2*xSqr,2*ySqr,xSqr,ySqr])
-	screen.blit(font.render(timeStr,False,black), ((int(xLen)/2)-3-int(len(timeS[0])*10) , (yLen/2)-(font.get_height()/2)))
+		pygame.draw.rect(screen, screen.get_at((2*xSqr+1,2*ySqr+1)), [2*xSqr,2*ySqr,xSqr,ySqr])
+		screen.blit(font.render(timeStr,False,black), ((int(xLen)/2)-3-int(len(timeS[0])*10) , (yLen/2)-(font.get_height()/2)))
 
 	pygame.time.wait(50)
 	pygame.display.update()
