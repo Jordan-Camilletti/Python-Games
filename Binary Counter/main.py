6cd23480d2a077d33b5d7d13e5e298cd052d8aa4
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-07-17 21:54:42 -0500

    Timer now centered

--- Binary Counter/main.py
+++ Binary Counter/main.py
@@ -14,13 +14,14 @@ pygame.init()
 white=(255,255,255)
 black=(0,0,0)
 finish=False
-count=0
+count=2000
 yLen=int(input("Enter window height\n"))
-ySqr=int(yLen/4)
+ySqr=int(yLen/5)
 xLen=int(yLen*8.86)
-xSqr=int(xLen/6)
+xSqr=int(xLen/5)
 
 timer=time.time()
+timeStr=""
 timeS=""
 font=pygame.font.SysFont('gillsans', 30)
 screen=pygame.display.set_mode((xLen,yLen))
@@ -35,12 +36,15 @@ while(not finish):
 	bCount=("{0:b}".format(count))[::-1]
 	for n in range(len(bCount)):
 		if(bCount[n]=="0"):
-			pygame.draw.rect(screen, white, [int(n/4)*xSqr,(n%4)*ySqr,xSqr,ySqr])
-		elif(bCount[n]=="1" and screen.get_at((int(n/4)*xSqr,(n%4)*ySqr)) == (255,255,255,255)):#This last part makes colors less 'ravey'
-			pygame.draw.rect(screen, (randint(0,255),randint(0,255),randint(0,255)), [int(n/4)*xSqr,(n%4)*ySqr,xSqr,ySqr])
+			pygame.draw.rect(screen, white, [int(n/5)*xSqr,(n%5)*ySqr,xSqr,ySqr])
+		elif(bCount[n]=="1" and screen.get_at((int(n/5)*xSqr,(n%5)*ySqr)) == (255,255,255,255)):#This last part makes colors less 'ravey'
+			pygame.draw.rect(screen, (randint(0,255),randint(0,255),randint(0,255)), [int(n/5)*xSqr,(n%5)*ySqr,xSqr,ySqr])
 	
 	timeS=str(time.time()-timer).split(".")
-	screen.blit(font.render(timeS[0]+"."+timeS[1][:2],False,black), (0,0))
+	timeStr=timeS[0]+"."+timeS[1][:2]#This makes sure that the time is cut off to 2 decimal places
+
+	pygame.draw.rect(screen, screen.get_at((2*xSqr+1,2*ySqr+1)), [2*xSqr,2*ySqr,xSqr,ySqr])
+	screen.blit(font.render(timeStr,False,black), (int(xLen)/2 , (yLen/2)-(font.get_height()/2)))
 
 	pygame.time.wait(50)
 	pygame.display.update()
