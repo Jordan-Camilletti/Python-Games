a60fa78906fdb0c4dc694924dc51a01ae509eab7
Jordan <jordan.camilletti@gmail.com>
2019-10-19 23:07:50 -0500

    .

--- Population-1/main.py
+++ Population-1/main.py
@@ -13,14 +13,14 @@ green=(0,255,0)
 blue=(0,0,255)
 finish=False
 
-screen=pygame.display.set_mode((400,300))
+screen=pygame.display.set_mode((800,600))
 screen.fill(white)
 pygame.display.set_caption("Population 1")
 
 while(not finish):
 	for event in pygame.event.get():
-		#if(event.type == pygame.QUIT):
-			#finish=True
+		if(event.type == pygame.QUIT):
+			finish=True
 		if(event.type == pygame.MOUSEBUTTONDOWN):
 			finish=True
 	screen.fill(white)
