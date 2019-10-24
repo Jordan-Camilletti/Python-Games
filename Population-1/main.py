3a148ee8dde31375c095f6956e1d20b06e73131b
Jordan <jordan.camilletti@gmail.com>
2019-10-24 00:00:13 -0500

    Added basic movement options

--- Population-1/main.py
+++ Population-1/main.py
@@ -6,6 +6,7 @@ import pygame
 
 print("")
 pygame.init()
+clock=pygame.time.Clock()
 white=(255,255,255)
 black=(0,0,0)
 red=(255,0,0)
@@ -13,9 +14,9 @@ green=(0,255,0)
 blue=(0,0,255)
 star=(10,10,244)
 finish=False
+
 screenXLen=800
 screenYLen=600
-
 screen=pygame.display.set_mode((screenXLen,screenYLen))
 screen.fill(black)
 pygame.display.set_caption("Population 1")
@@ -28,11 +29,19 @@ while(not finish):
 		if(event.type == pygame.QUIT):
 			finish=True
 		if(event.type == pygame.KEYDOWN):
-			while(event.key == pygame.K_a and playerXPos>0):
+			if(event.key == pygame.K_w and playerYPos>0):
+				playerYPos-=min(2,playerYPos)
+			if(event.key == pygame.K_a and playerXPos>0):
 				playerXPos-=min(2,playerXPos)
+			if(event.key == pygame.K_s and playerYPos<screenYLen*0.99):
+				playerYPos+=min(2,screenYLen-playerYPos)
+			if(event.key == pygame.K_d and playerXPos<screenXLen*0.99):
+				playerXPos+=min(2,screenXLen-playerXPos)
 	screen.fill(black)
 	pygame.draw.rect(screen,white,[playerXPos,playerYPos,(screenXLen*0.02),(screenXLen*0.02)])
 	pygame.display.update()
+	
+	clock.tick(45)
 	
 pygame.quit()
 quit()
