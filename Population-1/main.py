348bb3afba387204e872337b1d6f2b8ada159549
Alan <jordan.camilletti@gmail.com>
2019-10-29 15:14:13 -0500

    AI now moves much more smoothly

--- Population-1/main.py
+++ Population-1/main.py
@@ -29,6 +29,8 @@ screen.fill(black)
 pygame.display.set_caption("Population 1")
 
 def AIMove(ballY, paddleY, paddleLen, screenY, speed):
+	if(paddleY<ballY and abs(paddleY-ballY)<paddleLen/2):
+		return(paddleY)
 	if(paddleY>ballY):
 		return(paddleY-speed)
 	if(paddleY+paddleLen<screenY):
@@ -41,7 +43,7 @@ while(not finish):
 	paddle1=(screenYLen/2)-(paddleLength/2)
 	paddle2=(screenYLen/2)-(paddleLength/2)
 	movementX=ballSpeed-(2*ballSpeed*random.randint(0,1))
-	movementY=0.02*ballSpeed*random.randint(0,100)
+	movementY=0
 	ballXPos=(screenXLen/2)-(screenXLen*0.01)
 	ballYPos=(screenYLen/2)-(screenYLen*0.01)
 	while(match):
@@ -54,14 +56,14 @@ while(not finish):
 			paddle1-=paddleSpeed
 		if(keys[pygame.K_s] and paddle1+paddleLength<screenYLen):
 			paddle1+=paddleSpeed
-		paddle2=AIMove(ballYPos,paddle2,paddleLength,screenYLen,paddleSpeed)
+		paddle2=AIMove(ballYPos,paddle2,paddleLength,screenYLen,paddleSpeed+1)
 
 		if(abs(ballXPos-(screenXLen*0.1))<20 and ballYPos-paddle1<paddleLength and ballYPos-paddle1>=0):
 			movementX=(-1*movementX)+1
-			movementY=0.02*ballSpeed*random.randint(-100,100)
+			movementY=0.2*(ballYPos-paddle1-(paddleLength/2))
 		if(abs(ballXPos-(screenXLen-(screenXLen*0.125)))<20 and ballYPos-paddle2<paddleLength and ballYPos-paddle2>=0):
 			movementX=(-1*movementX)-1
-			movementY=0.02*ballSpeed*random.randint(-100,100)
+			movementY=0.2*(ballYPos-paddle2-(paddleLength/2))
 		ballXPos+=movementX
 		ballYPos+=movementY
 		if(ballXPos<0):
